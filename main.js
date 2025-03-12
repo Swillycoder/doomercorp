7e009bd477f43fe4022bbe8a30a4ce11c37f15f5
Edward 'Ted <159036536+Swillycoder@users.noreply.github.com>
2025-03-12 20:10:49 +0000

    Update main.js

--- main.js
+++ main.js
@@ -59,7 +59,7 @@ function typeText() {
 function fillScreen() {
     ctx.fillStyle = "black"; // Change this to any color you want
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(gin_img, 180, 0)
+    ctx.drawImage(gin_img, 80, 0)
 }
 
 typeText();
